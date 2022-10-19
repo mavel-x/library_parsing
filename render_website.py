2ba742404991217f0168c6679fd6fac924dd5a75
mavel <p.m.berdnikov@gmail.com>
2022-10-19 19:28:27 +0300

    split books into pages

--- render_website.py
+++ render_website.py
@@ -3,11 +3,12 @@ from pathlib import Path
 
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 from livereload import Server
+from more_itertools import chunked
 
 BASE_DIR = Path(__file__).parent
-WEB_DIR = Path('web')
-IMAGE_DIR = Path('images')
-BOOK_DIR = Path('books')
+WEB_DIR = BASE_DIR / Path('web')
+IMAGE_DIR = Path('static/images')
+BOOK_DIR = Path('static/books')
 
 BOOK_INFO_FILE = BASE_DIR / 'saved_books.json'
 
@@ -19,21 +20,25 @@ def render():
     )
     template = env.get_template(f'web/template.html')
 
-    with open(BOOK_INFO_FILE, 'r') as f:
-        books = json.load(f)
+    with open(BOOK_INFO_FILE, 'r') as file:
+        books = json.load(file)
 
     # convert paths from absolute into relative for web serving
     for book in books:
         full_image_path = Path(book['img_src'])
-        book['img_src'] = IMAGE_DIR / full_image_path.name
+        book['img_src'] = Path('..') / IMAGE_DIR / full_image_path.name
         full_txt_path = Path(book['book_path'])
-        book['book_path'] = BOOK_DIR / full_txt_path.name
+        book['book_path'] = Path('..') / BOOK_DIR / full_txt_path.name
 
-    rendered_page = template.render(
-        books=books,
-    )
-    with open('web/index.html', 'w', encoding="utf8") as file:
-        file.write(rendered_page)
+    page_dir = WEB_DIR / 'pages'
+    page_dir.mkdir(exist_ok=True)
+
+    for number, page_books in enumerate(chunked(books, 20), 1):
+        rendered_page = template.render(
+            books=page_books,
+        )
+        with open(page_dir / f'index{number}.html', 'w', encoding="utf8") as file:
+            file.write(rendered_page)
 
 
 def main():
