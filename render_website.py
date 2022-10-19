d312d105e1a5293bd5a242dfbe9eededa12d6d44
mavel <p.m.berdnikov@gmail.com>
2022-10-19 19:01:48 +0300

    add links to book txt files

--- render_website.py
+++ render_website.py
@@ -7,6 +7,7 @@ from livereload import Server
 BASE_DIR = Path(__file__).parent
 WEB_DIR = Path('web')
 IMAGE_DIR = Path('images')
+BOOK_DIR = Path('books')
 
 BOOK_INFO_FILE = BASE_DIR / 'saved_books.json'
 
@@ -21,9 +22,12 @@ def render():
     with open(BOOK_INFO_FILE, 'r') as f:
         books = json.load(f)
 
+    # convert paths from absolute into relative for web serving
     for book in books:
-        full_path = Path(book['img_src'])
-        book['img_src'] = IMAGE_DIR / full_path.name
+        full_image_path = Path(book['img_src'])
+        book['img_src'] = IMAGE_DIR / full_image_path.name
+        full_txt_path = Path(book['book_path'])
+        book['book_path'] = BOOK_DIR / full_txt_path.name
 
     rendered_page = template.render(
         books=books,
@@ -33,6 +37,8 @@ def render():
 
 
 def main():
+    render()
+
     server = Server()
     server.watch('web/template.html', render)
     server.serve(root=WEB_DIR)
