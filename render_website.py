e30462bc9625a76aa25e49f91927beebfe969410
mavel <p.m.berdnikov@gmail.com>
2022-10-19 18:41:54 +0300

    move website files to a separate directory

--- render_website.py
+++ render_website.py
@@ -5,6 +5,7 @@ from jinja2 import Environment, FileSystemLoader, select_autoescape
 from livereload import Server
 
 BASE_DIR = Path(__file__).parent
+WEB_DIR = Path('web')
 IMAGE_DIR = Path('images')
 
 BOOK_INFO_FILE = BASE_DIR / 'saved_books.json'
@@ -15,7 +16,7 @@ def render():
         loader=FileSystemLoader(BASE_DIR),
         autoescape=select_autoescape(['html', 'xml'])
     )
-    template = env.get_template('template.html')
+    template = env.get_template(f'web/template.html')
 
     with open(BOOK_INFO_FILE, 'r') as f:
         books = json.load(f)
@@ -27,14 +28,14 @@ def render():
     rendered_page = template.render(
         books=books,
     )
-    with open('index.html', 'w', encoding="utf8") as file:
+    with open('web/index.html', 'w', encoding="utf8") as file:
         file.write(rendered_page)
 
 
 def main():
     server = Server()
-    server.watch('template.html', render)
-    server.serve(root=BASE_DIR)
+    server.watch('web/template.html', render)
+    server.serve(root=WEB_DIR)
 
 
 if __name__ == "__main__":
